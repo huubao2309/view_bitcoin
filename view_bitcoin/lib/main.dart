355d4ec1f9d9f1017b65a7a9fbb928840a0ee8ea
Huu Bao Nguyen <huubao2309@gmail.com>
2020-03-12 00:07:53 +0700

    commit for read data

--- view_bitcoin/lib/main.dart
+++ view_bitcoin/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:html/parser.dart';
+import 'package:http/http.dart';
+
+import 'model/bitcoin.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,11 +25,72 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ViewBitcoin extends StatelessWidget {
+class ViewBitcoin extends StatefulWidget {
+  @override
+  _ViewBitcoinState createState() => _ViewBitcoinState();
+}
+
+class _ViewBitcoinState extends State<ViewBitcoin> {
+  final coinData = <BitCoin>[];
+
+  @override
+  void initState() {
+    super.initState();
+    getCoinInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Flutter Demo Home Page'),
-    );
+    return Container();
   }
+
+  void getCoinInfo() async {
+    final client = Client();
+    const linkURL = 'https://kenniex.com/';
+    var response = await client.get(linkURL);
+    final document = parse(response.body);
+    final divStr = document.querySelectorAll("div");
+
+    for (final divParent in divStr) {
+      try {
+        if (divParent.className == 'main mt-10') {
+          for (final divChildren in divParent.children) {
+            if (divChildren.className == 'tab-content') {
+              for (final divChildren1 in divChildren.children) {
+                if (divChildren1.className == 'tab-pane fade in active') {
+                  for (final divChildren2 in divChildren1.children) {
+                    if (divChildren2.className ==
+                        'table-responsive coin-list-table') {
+                      for (final table in divChildren2.children) {
+                        if (table.className ==
+                            'table table-condensed table-hover') {
+                          var abc = table;
+                          var abcd = table.children;
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      } catch (ex) {
+        print(ex);
+      }
+    }
+  }
+
+  // void getTableContainCoin(List<Element> list) {
+  //   for (final i in list) {
+  //     try {
+  //       if (i.class == "table table-condensed table-hover") {
+  //         final table = i.querySelectorAll("table");
+  //         final trs = table.querySelectorAll('tr');
+  //       }
+  //     } catch (ex) {
+  //       print(ex);
+  //     }
+  //   }
+  // }
 }
---
+++ view_bitcoin/lib/model/bitcoin.dart
@@ -0,0 +1,7 @@
+class BitCoin {
+  String coin;
+  String change;
+  String price;
+
+  BitCoin(this.coin, this.change, this.price);
+}
